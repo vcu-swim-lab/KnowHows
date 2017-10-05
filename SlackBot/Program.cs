1fa5ddcc9930c972a28a0c07cc342ce9c44f53b0
Seefo <robbie.uvanni@gmail.com>
2017-10-05 04:49:24 -0400

    Initial commit

---
+++ SlackBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SlackClient test = new SlackClient("clientId", "clientSecret", "teamName");
+        }
+    }
+}
