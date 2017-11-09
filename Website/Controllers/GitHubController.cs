df27be37408676c8544e154b48bf9055801a2424
Seefo <robbie.uvanni@gmail.com>
2017-11-09 11:51:22 -0500

    Initial work on web services

---
+++ Website/Controllers/GitHubController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace Website.Controllers
+{
+    public class GitHubController : ApiController
+    {
+        private const string GITHUB_APP_CLIENT_ID = "";
+        private const string GITHUB_APP_CLIENT_SECRET = "";
+        private const string GITHUB_APP_CLIENT_SCOPE = "user notifications repo";
+
+        private const string GITHUB_APP_OAUTH_URL = "https://github.com/login/oauth/authorize";
+        private const string GITHUB_APP_OAUTH_REDIRECT_URL = "https://github.com/login/oauth/authorize";
+
+        public IHttpActionResult Authenticate(string bearer, string scope, string access_token)
+        {
+            Console.WriteLine("Received GitHub OAuth: {0}, {1}, {2}", bearer, scope, access_token);
+            return Ok();
+        }
+
+        public string GetOAuthURL()
+        {
+            return GITHUB_APP_OAUTH_URL +
+                String.Format("?client_id={0}&redirect_uri={1}&scope={2}&state={3}&allow_signup={4}",
+                GITHUB_APP_CLIENT_ID,
+                GITHUB_APP_OAUTH_REDIRECT_URL,
+                GITHUB_APP_CLIENT_SCOPE,
+                "state",
+                "true");
+        }
+    }
+}
---
+++ Website/Controllers/SlackController.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Website.Controllers
+{
+    public class SlackController : ApiController
+    {
+        private const string SLACK_APP_CLIENT_ID = "";
+        private const string SLACK_APP_CLIENT_SECRET = "";
+
+        public class SlashCommand
+        {
+            public string token, team_id, team_domain, enterprise_id, enterprise_name, channel_id, channel_name, user_id, user_name, command, text, response_url, trigger_id;
+            public SlashCommand(FormDataCollection data)
+            {
+                this.token              = data.Get("token");
+                this.team_id            = data.Get("team_id");
+                this.team_domain        = data.Get("team_domain");
+                this.enterprise_id      = data.Get("enterprise_id");
+                this.enterprise_name    = data.Get("enterprise_name");
+                this.channel_id         = data.Get("channel_id");
+                this.channel_name       = data.Get("channel_name");
+                this.user_id            = data.Get("user_id");
+                this.user_name          = data.Get("user_name");
+                this.command            = data.Get("command");
+                this.text               = data.Get("text");
+                this.response_url       = data.Get("response_url");
+                this.trigger_id         = data.Get("trigger_id");
+            }
+        }
+
+        private class OAuthRequest
+        {
+            public string client_id, client_secret, code, redirect_uri;
+        }
+
+        private class OAuthResponse
+        {
+            public string access_token, scope;
+        }
+
+        public IHttpActionResult ProcessMessage(FormDataCollection data)
+        {
+            // @TODO: there should be a better way of deserializing this
+            SlashCommand command = new SlashCommand(data);
+
+            // @TODO: process slash command
+
+            return Ok();
+        }
+
+        public IHttpActionResult Authenticate(string code, string state)
+        {
+            Console.WriteLine("Receieved install: {0} {1}", code, state);
+
+            // @TODO: save the token and associate it with something
+            OAuthResponse response = RequestAccessToken(code);
+            Console.WriteLine("Received access token for code {0}, access token: {1}", code, response.access_token);
+
+            return Redirect("google.com");
+        }
+
+        private OAuthResponse RequestAccessToken(string code)
+        {
+            WebClient client = new WebClient();
+            client.Headers["Accept"] = "application/json";
+
+            string uri = String.Format("https://slack.com/api/oauth.access?client_id={0}&client_secret={1}&code={2}", SLACK_APP_CLIENT_ID, SLACK_APP_CLIENT_SECRET, code);
+            string response = client.DownloadString(new Uri(uri));
+
+            try { return JsonConvert.DeserializeObject<OAuthResponse>(response); }
+            catch (Exception ex)
+            {
+                throw new Exception(String.Format("Failed to process OAuth response to request for code {0}", code));
+            }
+        }
+    }
+}
