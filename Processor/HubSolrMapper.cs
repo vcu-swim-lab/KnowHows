63eed29e43863a785e30bed739ee963c3461c512
broem <wacbleach@gmail.com>
2017-11-28 13:59:40 -0500

    Processing init
    processor init. this will manage the mapping/algos between github and solr.

---
+++ Processor/HubSolrMapper.cs
@@ -0,0 +1,49 @@
+﻿using GitAuth;
+using SolrServices;
+using System.Collections.Generic;
+
+namespace Processor
+{
+    public class HubSolrMapper
+    {
+        /// <summary>
+        /// This will take a list of GitHub commits and convert them into a List 
+        /// ready to be put into Solr. 
+        /// </summary>
+        /// <param name="coms">Commits</param>
+        /// <param name="toke">access token</param>
+        /// <returns></returns>
+        public List<CodeDoc> HubToSolr(List<GithubCommit> coms, string toke)
+        {
+            // TODO: get channel Id from slack to filter with later. 
+            List<CodeDoc> solrStuff = new List<CodeDoc>();
+            int num = 30;
+            int blah = 1;
+            foreach (var commit in coms)
+            {
+                CodeDoc code = new CodeDoc();
+                code.Author_Date = commit.author_date.DateTime;
+                code.Author_Name = commit.author_name;
+                code.Blob_Url = commit.blob_url;
+                code.Committer_Name = commit.committer_name;
+                code.Patch = commit.patch;
+                code.Content = commit.content;
+                code.Filename = commit.filename;
+                code.Id = "somechannelthingyiguess" + num;
+                code.Accesstoken = toke;
+                code.Previous_File_Name = commit.previous_file_name;
+                code.Raw_Url = commit.raw_url;
+                code.Sha = commit.sha;
+                code.Channel = "channel" + blah;
+                if (num % 10 == 0)
+                {
+                    blah += 2;
+                }
+                num++;
+                solrStuff.Add(code);
+            }
+
+            return solrStuff;
+        }
+    }
+}
---
+++ Processor/Manager.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Processor
+{
+    public class Manager
+    {
+        public string SolrClean()
+        {
+            // remove all documents: "localhost:8983/solr/new_core/update?commit=true" -H "Content-Type: text/xml" --data-binary "<delete><query>*:*</query></delete>"
+            return "todo";
+        }
+    }
+}
