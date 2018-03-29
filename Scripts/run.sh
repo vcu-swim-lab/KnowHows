22cbe262c12acc25c09579743f4c8cec49b04f18
seefo <robbie.uvanni@gmail.com>
2018-03-29 12:15:13 +0000

    Updated dockerfile and build scripts

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,8 +1,7 @@
 if [ -d "Website" ]; then
-  cd Website
   docker kill knowhows
   docker rm knowhows
-  docker run -p 53222:53222 -d --name knowhows knowhows
+  docker run -p 53222:53222 -v $PWD/data/:/dotnetapp/Website/data/:z -d --name knowhows knowhows
 else
   echo "Run this script from the root directory of this project (i.e. Website folder should be present)"
 fi
